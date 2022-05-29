f0877cc44f382867fab65158a8b9dd5b2565e337
CamiloNaOr <camilonaor@unisabana.edu.co>
2022-05-28 19:29:44 -0500

    subir

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,55 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Biblioteca.Api.Data;
+using Biblioteca.Api.Modelo;
+
+namespace Biblioteca.Api.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class ClienteController : ControllerBase
+    {
+
+        private readonly BibliotecaApiContext _context;
+        public ClienteController(BibliotecaApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ConsultaClientes()
+        {
+            return Ok(await _context.Cliente.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AgregarCliente(Cliente cliente)
+        {
+            _context.Cliente.Add(cliente);
+            await _context.SaveChangesAsync();
+            return Ok(cliente);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> ActualizarCliente(Cliente cliente)
+        {
+            _context.Update(cliente);
+            await _context.SaveChangesAsync();
+            return Ok(cliente);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteCliente(Cliente cliente)
+        {
+            _context.Remove(cliente);
+            await _context.SaveChangesAsync();
+            return Ok(cliente);
+        }
+
+    }
+}
---
+++ Controllers/ProductoController.cs
@@ -0,0 +1,56 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Biblioteca.Api.Data;
+using Biblioteca.Api.Modelo;
+
+namespace Biblioteca.Api.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class ProductoController : ControllerBase
+    {
+
+        private readonly BibliotecaApiContext _context;
+        public ProductoController(BibliotecaApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ConsultaProductos()
+        {
+            return Ok(await _context.Producto.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AgregarProducto(Producto producto)
+        {
+            _context.Producto.Add(producto);
+            await _context.SaveChangesAsync();
+            return Ok(producto);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> ActualizarProducto(Producto producto)
+        {
+            _context.Update(producto);
+            await _context.SaveChangesAsync();
+            return Ok(producto);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteProducto(Producto producto)
+        {
+            _context.Remove(producto);
+            await _context.SaveChangesAsync();
+            return Ok(producto);
+        }
+
+    }
+}
+
